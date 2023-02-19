2ca7e167a13b9fde2aea7d12c629fedbf2a6087e
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-02-19 17:16:17 -0500

    Run task 3 code

---
+++ mavenproject1/src/main/java/com/mycompany/Lab4/Task03.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Lab4;
+
+/**
+ *
+ * @author Ranveer Singh PC
+ */
+public class Task03 {
+    int fib(int n)
+
+    {
+
+       if (n <= 0) // base case
+
+            return 0;
+
+      else if (n == 1) // base case
+
+             return 1;
+
+       else
+      return fib(n-1) + fib(n-2);
+    }
+}
+
+
+
+
