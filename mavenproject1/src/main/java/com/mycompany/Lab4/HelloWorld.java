82d28d2dfd198e8ce418f20c2e66bba56f9dd709
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-02-19 18:40:19 -0500

    Write a function to print Hello World n times recursively

---
+++ mavenproject1/src/main/java/com/mycompany/Lab4/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Lab4;
+
+/**
+ *
+ * @author Ranveer Singh
+ */
+public class HelloWorld {
+  
+  public static void main(String[]args, int printMessage)  {
+      
+      int n = 1;
+      printMessage=(n);
+  }
+    public static void printMessage(int n) {
+    }
+          
+    
+    
+}
