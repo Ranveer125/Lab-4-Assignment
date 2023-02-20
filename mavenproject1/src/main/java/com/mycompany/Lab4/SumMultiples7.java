76b372e4ce473285ffe728edcd860af9fe140b2e
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-02-19 19:36:18 -0500

    Write function to return sum of all numbers between n1 and n2

---
+++ mavenproject1/src/main/java/com/mycompany/Lab4/SumMultiples7.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Lab4;
+
+/**
+ *
+ * @author Ranveer Singh 
+ */
+public class SumMultiples7 {
+     public static void main(String args[]){
+         int n1=14, n2=21,sum;
+         sum=n1+n2;
+         System.out.print("Sum of n1+n2"+ 7);
+     }
+      static int findSum(int n1, int n2){
+          
+          int sum = n1+n2;
+          
+         return 7;
+          
+      }
+        
+}
+
