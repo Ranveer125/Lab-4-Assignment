41a6816baaf32bab6b811fb6e3542614b67d88ed
Ranveer Singh PC <Ranveer Singh PC@RanveerSingh-PC.lan>
2023-02-19 19:51:40 -0500

    Write a function to implement binary search algorithm recursively

---
+++ mavenproject1/src/main/java/com/mycompany/Lab4/BinarySearch.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Lab4;
+
+/**
+ *
+ * @author Ranveer Singh 
+ */
+public class BinarySearch {
+   int binarySearch(int array[], int target, int low, int high) {
+
+    if (high >= low) {
+      int mid = low + (high - low) / 2;
+
+      
+      if (array[mid] == target)
+          
+        return mid;
+                   //Check to see if the mid target search
+      
+      if (array[mid] > target)
+     return binarySearch(array, target, low, mid - 1);
+
+      
+      return binarySearch(array, target, mid + 1, high);
+    }
+
+    return -1;
+  }
+}
